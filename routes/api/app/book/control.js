e05372b309a56afa153befebb114ff3f7d9fc6fa
eclatchung <enrichir97@gmail.com>
2019-05-31 11:17:59 +0900

    0531

--- routes/api/app/book/control.js
+++ routes/api/app/book/control.js
@@ -42,57 +42,56 @@ exports.showtime=(req,res)=>{
     function showpretty(booktime){
 
         var result = [];
-        var ob={};
         var num = 0;
 
-        if(booktime.AM_00시00분==0) {ob.time = 'AM_00시00분'; result[num]=ob; num++;}
-        if(booktime.AM_00시30분==0) {ob.time  = 'AM_00시30분'; result[num]=ob; num++;}
-        if(booktime.AM_01시00분==0) {ob.time  = 'AM_01시00분';result[num]=ob; num++;}
-        if(booktime.AM_01시30분==0) {ob.time  = 'AM_01시30분';result[num]=ob; num++;}
-        if(booktime.AM_02시00분==0) {ob.time  = 'AM_02시00분';result[num]=ob; num++;}
-        if(booktime.AM_02시30분==0) {ob.time  = 'AM_02시30분';result[num]=ob; num++;}
-        if(booktime.AM_03시00분==0) {ob.time  = 'AM_03시00분';result[num]=ob; num++;}
-        if(booktime.AM_03시30분==0) {ob.time  = 'AM_03시30분';result[num]=ob; num++;}
-        if(booktime.AM_04시00분==0) {ob.time  = 'AM_04시00분';result[num]=ob; num++;}
-        if(booktime.AM_04시30분==0) {ob.time  = 'AM_04시30분';result[num]=ob; num++;}
-        if(booktime.AM_05시00분==0) {ob.time  = 'AM_05시00분';result[num]=ob; num++;}
-        if(booktime.AM_05시30분==0) {ob.time  = 'AM_05시30분';result[num]=ob; num++;}
-        if(booktime.AM_06시00분==0) {ob.time  = 'AM_06시00분';result[num]=ob; num++;}
-        if(booktime.AM_06시30분==0) {ob.time  = 'AM_06시30분';result[num]=ob; num++;}
-        if(booktime.AM_07시00분==0) {ob.time  = 'AM_07시00분'; result[num]=ob; num++;}
-        if(booktime.AM_07시30분==0) {ob.time  = 'AM_07시30분';result[num]=ob; num++;}
-        if(booktime.AM_08시00분==0) {ob.time  = "AM_08시00분";result[num]=ob; num++;}
-        if(booktime.AM_08시30분==0) {ob.time  = 'AM_08시30분';result[num]=ob; num++;}
-        if(booktime.AM_09시00분==0) {ob.time  = 'AM_09시00분';result[num]=ob; num++;}
-        if(booktime.AM_09시30분==0) {ob.time = 'AM_09시30분'; result[num]=ob; num++;}
-        if(booktime.AM_10시00분==0) {ob.time = 'AM_10시00분';result[num]=ob; num++;}
-        if(booktime.AM_10시30분==0) {ob.time = 'AM_10시30분'; result[num]=ob; num++;}
-        if(booktime.AM_11시00분==0) {ob.time = 'AM_11시00분'; result[num]=ob; num++;}
-        if(booktime.AM_11시30분==0) {ob.time = 'AM_11시30분'; result[num]=ob; num++;}
-        if(booktime.PM_12시00분==0) {ob.time = 'PM_12시00분'; result[num]=ob; num++;}
-        if(booktime.PM_12시30분==0) {ob.time = 'PM_12시30분'; result[num]=ob; num++;}
-        if(booktime.PM_01시00분==0) {ob.time = 'PM_01시00분'; result[num]=ob; num++;}
-        if(booktime.PM_01시30분==0) {ob.time = 'PM_01시30분'; result[num]=ob; num++;}
-        if(booktime.PM_02시00분==0) {ob.time = 'PM_02시00분'; result[num]=ob; num++;}
-        if(booktime.PM_02시30분==0) {ob.time = 'PM_02시30분'; result[num]=ob; num++;}
-        if(booktime.PM_03시00분==0) {ob.time = 'PM_03시00분'; result[num]=ob; num++;}
-        if(booktime.PM_03시30분==0) {ob.time = 'PM_03시30분'; result[num]=ob; num++;}
-        if(booktime.PM_04시00분==0) {ob.time = 'PM_04시00분'; result[num]=ob; num++;}
-        if(booktime.PM_04시30분==0) {ob.time = 'PM_04시30분'; result[num]=ob; num++;}
-        if(booktime.PM_05시00분==0) {ob.time = 'PM_05시00분'; result[num]=ob; num++;}
-        if(booktime.PM_05시30분==0) {ob.time = 'PM_05시30분'; result[num]=ob; num++;}
-        if(booktime.PM_06시00분==0) {ob.time = 'PM_06시00분'; result[num]=ob; num++;}
-        if(booktime.PM_06시30분==0) {ob.time = 'PM_06시30분'; result[num]=ob; num++;}
-        if(booktime.PM_07시00분==0) {ob.time = 'PM_07시00분'; result[num]=ob; num++;}
-        if(booktime.PM_07시30분==0) {ob.time = 'PM_07시30분'; result[num]=ob; num++;}
-        if(booktime.PM_08시00분==0) {ob.time = 'PM_08시00분'; result[num]=ob; num++;}
-        if(booktime.PM_08시30분==0) {ob.time = 'PM_08시30분'; result[num]=ob; num++;}
-        if(booktime.PM_09시00분==0) {ob.time = 'PM_09시00분'; result[num]=ob; num++;}
-        if(booktime.PM_09시30분==0) {ob.time = 'PM_09시30분'; result[num]=ob; num++;}
-        if(booktime.PM_10시00분==0) {ob.time = 'PM_10시00분'; result[num]=ob; num++;}
-        if(booktime.PM_10시30분==0) {ob.time = 'PM_10시30분'; result[num]=ob; num++;}
-        if(booktime.PM_11시00분==0) {ob.time = 'PM_11시00분'; result[num]=ob; num++;}
-        if(booktime.PM_11시30분==0) {ob.time = 'PM_11시30분'; result[num]=ob; num++;}
+        if(booktime.AM_00시00분==0) {var ob={}; ob.time = 'AM_00시00분'; result[num]=ob; num++;}
+        if(booktime.AM_00시30분==0) {var ob={}; ob.time  = 'AM_00시30분'; result[num]=ob; num++;}
+        if(booktime.AM_01시00분==0) {var ob={}; ob.time  = 'AM_01시00분';result[num]=ob; num++;}
+        if(booktime.AM_01시30분==0) {var ob={}; ob.time  = 'AM_01시30분';result[num]=ob; num++;}
+        if(booktime.AM_02시00분==0) {var ob={}; ob.time  = 'AM_02시00분';result[num]=ob; num++;}
+        if(booktime.AM_02시30분==0) {var ob={};ob.time  = 'AM_02시30분';result[num]=ob; num++;}
+        if(booktime.AM_03시00분==0) {var ob={};ob.time  = 'AM_03시00분';result[num]=ob; num++;}
+        if(booktime.AM_03시30분==0) {var ob={};ob.time  = 'AM_03시30분';result[num]=ob; num++;}
+        if(booktime.AM_04시00분==0) {var ob={};ob.time  = 'AM_04시00분';result[num]=ob; num++;}
+        if(booktime.AM_04시30분==0) {var ob={};ob.time  = 'AM_04시30분';result[num]=ob; num++;}
+        if(booktime.AM_05시00분==0) {var ob={};ob.time  = 'AM_05시00분';result[num]=ob; num++;}
+        if(booktime.AM_05시30분==0) {var ob={};ob.time  = 'AM_05시30분';result[num]=ob; num++;}
+        if(booktime.AM_06시00분==0) {var ob={};ob.time  = 'AM_06시00분';result[num]=ob; num++;}
+        if(booktime.AM_06시30분==0) {var ob={};ob.time  = 'AM_06시30분';result[num]=ob; num++;}
+        if(booktime.AM_07시00분==0) {var ob={};ob.time  = 'AM_07시00분'; result[num]=ob; num++;}
+        if(booktime.AM_07시30분==0) {var ob={};ob.time  = 'AM_07시30분';result[num]=ob; num++;}
+        if(booktime.AM_08시00분==0) {var ob={};ob.time  = "AM_08시00분";result[num]=ob; num++;}
+        if(booktime.AM_08시30분==0) {var ob={};ob.time  = 'AM_08시30분';result[num]=ob; num++;}
+        if(booktime.AM_09시00분==0) {var ob={};ob.time  = 'AM_09시00분';result[num]=ob; num++;}
+        if(booktime.AM_09시30분==0) {var ob={};ob.time = 'AM_09시30분'; result[num]=ob; num++;}
+        if(booktime.AM_10시00분==0) {var ob={};ob.time = 'AM_10시00분';result[num]=ob; num++;}
+        if(booktime.AM_10시30분==0) {var ob={};ob.time = 'AM_10시30분'; result[num]=ob; num++;}
+        if(booktime.AM_11시00분==0) {var ob={};ob.time = 'AM_11시00분'; result[num]=ob; num++;}
+        if(booktime.AM_11시30분==0) {var ob={};ob.time = 'AM_11시30분'; result[num]=ob; num++;}
+        if(booktime.PM_12시00분==0) {var ob={};ob.time = 'PM_12시00분'; result[num]=ob; num++;}
+        if(booktime.PM_12시30분==0) {var ob={};ob.time = 'PM_12시30분'; result[num]=ob; num++;}
+        if(booktime.PM_01시00분==0) {var ob={};ob.time = 'PM_01시00분'; result[num]=ob; num++;}
+        if(booktime.PM_01시30분==0) {var ob={};ob.time = 'PM_01시30분'; result[num]=ob; num++;}
+        if(booktime.PM_02시00분==0) {var ob={};ob.time = 'PM_02시00분'; result[num]=ob; num++;}
+        if(booktime.PM_02시30분==0) {var ob={};ob.time = 'PM_02시30분'; result[num]=ob; num++;}
+        if(booktime.PM_03시00분==0) {var ob={};ob.time = 'PM_03시00분'; result[num]=ob; num++;}
+        if(booktime.PM_03시30분==0) {var ob={};ob.time = 'PM_03시30분'; result[num]=ob; num++;}
+        if(booktime.PM_04시00분==0) {var ob={};ob.time = 'PM_04시00분'; result[num]=ob; num++;}
+        if(booktime.PM_04시30분==0) {var ob={};ob.time = 'PM_04시30분'; result[num]=ob; num++;}
+        if(booktime.PM_05시00분==0) {var ob={};ob.time = 'PM_05시00분'; result[num]=ob; num++;}
+        if(booktime.PM_05시30분==0) {var ob={};ob.time = 'PM_05시30분'; result[num]=ob; num++;}
+        if(booktime.PM_06시00분==0) {var ob={};ob.time = 'PM_06시00분'; result[num]=ob; num++;}
+        if(booktime.PM_06시30분==0) {var ob={};ob.time = 'PM_06시30분'; result[num]=ob; num++;}
+        if(booktime.PM_07시00분==0) {var ob={};ob.time = 'PM_07시00분'; result[num]=ob; num++;}
+        if(booktime.PM_07시30분==0) {var ob={};ob.time = 'PM_07시30분'; result[num]=ob; num++;}
+        if(booktime.PM_08시00분==0) {var ob={};ob.time = 'PM_08시00분'; result[num]=ob; num++;}
+        if(booktime.PM_08시30분==0) {var ob={};ob.time = 'PM_08시30분'; result[num]=ob; num++;}
+        if(booktime.PM_09시00분==0) {var ob={};ob.time = 'PM_09시00분'; result[num]=ob; num++;}
+        if(booktime.PM_09시30분==0) {var ob={};ob.time = 'PM_09시30분'; result[num]=ob; num++;}
+        if(booktime.PM_10시00분==0) {var ob={};ob.time = 'PM_10시00분'; result[num]=ob; num++;}
+        if(booktime.PM_10시30분==0) {var ob={};ob.time = 'PM_10시30분'; result[num]=ob; num++;}
+        if(booktime.PM_11시00분==0) {var ob={};ob.time = 'PM_11시00분'; result[num]=ob; num++;}
+        if(booktime.PM_11시30분==0) {var ob={};ob.time = 'PM_11시30분'; result[num]=ob; num++;}
 
         return result;
 
